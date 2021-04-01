f03b88feb6d302a474198ebf244308822d4bb422
fatihkaan22 <fatihkaan22@gmail.com>
2021-04-01 17:01:29 +0300

    bubble sort

---
+++ hw1/BubbleSort.asm
@@ -0,0 +1,72 @@
+.text
+
+main:
+
+	li $s3, 8								# size
+	addi $t0, $s3 -1				# size - 2
+
+L1:
+	li $t2, 0							# changed = false 
+	li $t3, 0							# i = 0
+	la $t5, test1
+L2:
+	bge $t3, $t0, L2Exit
+
+	lw $s5, 0($t5) 					# *ptr
+	lw $s6, 4($t5)  				# *(ptr + 1)
+	ble $s5, $s6, ifExit 		# if ( *ptr > * (ptr + 1) )
+	sw $s6, 0($t5) 					# ptr = ptr + 1
+	sw $s5, 4($t5) 					# ptr + 1 = ptr
+	li, $t2, 1							# changed = true 
+ifExit:
+	addi $t3, $t3, 1 				# ++i
+	addi $t5, $t5, 4 				# ++i
+	j L2
+L2Exit:
+
+	bne $t2, $zero, L1 				# while(changed)
+
+	la $a0, test1 					# arr
+	move $a1, $s3 					# size
+	jal printArr 						# print(arr, size)
+
+	j exit
+
+exit:
+	li $v0, 10							# system call to exit program
+	syscall
+
+# a0: arr, a1: size
+printArr:
+	move $t2, $a0 					# t2 = arr
+	move $t1, $a1 					# t1 = size
+printLoop:
+	ble $t1, $zero, ret_printArr
+	lw $a0, 0($t2)
+	li $v0, 1
+	syscall
+	la $a0, str_space
+	li $v0, 4
+	syscall
+	addi $t2, $t2, 4
+	addi $t1, $t1, -1
+	j printLoop
+ret_printArr:
+	jr $ra
+
+
+
+.data
+
+test1:	.word	41 67 34 0 69 24 78 58 	
+test2:	.word	62 64 5 45 81 27 61 91	
+test3:	.word	95 42 27 36 91 4 2 53	
+test4:	.word	92 82 21 16 18 95 47 26	
+test5:	.word	71 38 69 12 67 99 35 94	 
+test6:	.word	11 22 33 73 64 41 11	 
+
+arr:	.space	400					# MAX_SIZE=100
+
+str_space:	.asciiz " "
+str_size:	.asciiz "Enter number of integers: "
+str_enter:	.asciiz "Enter integers: "
