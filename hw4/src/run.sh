ee6ada1a23c1ceb2ed9c9370622f367c79108a46
fatihkaan22 <fatihkaan22@gmail.com>
2021-06-29 19:21:29 +0300

    example run case

--- hw4/src/run.sh
+++ hw4/src/run.sh
@@ -1,14 +1,19 @@
 make
 echo "> ./makeFileSystem 4 mySystem.data"
-./makeFileSystem 4 mySystem.data
+./makeFileSystem 0.5 mySystem.data
 echo "> ./fileSystemOper mySystem.data mkdir '\usr'"
 ./fileSystemOper mySystem.data mkdir '\usr'
 echo "> ./fileSystemOper mySystem.data mkdir '\usr\ysa'"
 ./fileSystemOper mySystem.data mkdir '\usr\ysa'
 echo "> ./fileSystemOper mySystem.data mkdir '\bin\ysa'"
 ./fileSystemOper mySystem.data mkdir '\bin\ysa'
-echo "> echo "contents of linuxFile.data" > linuxFile.data"
-echo "contents of linuxFile.data" > linuxFile.data
+echo "> echo 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.' > linuxFile.data"
+echo "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+" > linuxFile.data
 ./fileSystemOper mySystem.data write '\usr\ysa\file1' linuxFile.data
 echo "> ./fileSystemOper mySystem.data write '\usr\file2' linuxFile.data"
 ./fileSystemOper mySystem.data write '\usr\file2' linuxFile.data
