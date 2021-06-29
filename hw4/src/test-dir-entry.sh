6fa72da4cd9ff61f014535ad24da48db0099df49
fatihkaan22 <fatihkaan22@gmail.com>
2021-06-29 19:23:02 +0300

    test case

---
+++ hw4/src/test-dir-entry.sh
@@ -0,0 +1,29 @@
+clear &&
+make &&
+./makeFileSystem 0.5 mySystem.dat && ls -l mySystem.dat && hexdump mySystem.dat &&
+./fileSystemOper mySystem.dat mkdir "\dir01"
+./fileSystemOper mySystem.dat mkdir "\dir02"
+./fileSystemOper mySystem.dat mkdir "\dir03"
+./fileSystemOper mySystem.dat mkdir "\dir04"
+./fileSystemOper mySystem.dat mkdir "\dir05"
+./fileSystemOper mySystem.dat mkdir "\dir06"
+./fileSystemOper mySystem.dat mkdir "\dir07"
+./fileSystemOper mySystem.dat mkdir "\dir08"
+./fileSystemOper mySystem.dat mkdir "\dir09"
+./fileSystemOper mySystem.dat mkdir "\dir10"
+./fileSystemOper mySystem.dat mkdir "\dir11"
+./fileSystemOper mySystem.dat mkdir "\dir12"
+./fileSystemOper mySystem.dat mkdir "\dir13"
+./fileSystemOper mySystem.dat mkdir "\dir14"
+./fileSystemOper mySystem.dat mkdir "\dir15"
+./fileSystemOper mySystem.dat mkdir "\dir16"
+./fileSystemOper mySystem.dat mkdir "\dir17"
+./fileSystemOper mySystem.dat mkdir "\dir18"
+./fileSystemOper mySystem.dat mkdir "\dir19"
+./fileSystemOper mySystem.dat mkdir "\dir20"
+./fileSystemOper mySystem.dat mkdir "\dir21"
+./fileSystemOper mySystem.dat mkdir "\dir22"
+./fileSystemOper mySystem.dat mkdir "\dir23"
+./fileSystemOper mySystem.dat mkdir "\dir24"
+./fileSystemOper mySystem.dat dir '\'
+
